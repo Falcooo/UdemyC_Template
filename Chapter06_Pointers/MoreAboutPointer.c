d8b85fe9de995fdd4fee7e9fd4ab79555846cfe1
Falcooo <chiara.falcone@web.de>
2022-08-12 15:17:12 +0200

    Pointers

---
+++ Chapter06_Pointers/MoreAboutPointer.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void do_something(double param_in, double *param_out1, double *param_out2)
+{
+    //Wert von argument_out1 & argument_2 sind an der Speicheradresse
+    //&argument_out1 & &argument_out2 abgespeichert
+    //der Pointer zeigt auf die Adresse und ließt den dort liegenden Wert aus
+    *param_out1 += param_in;
+    *param_out2 += param_in;
+}
+
+int main()
+{
+
+    double argument_in = 1.0;
+    double argument_out1 = 2.0;
+    double argument_out2 = 3.0;
+
+    //in der Funktion muss eine Adresse übergeben werden, da in der
+    //Parameterleiste Pointer erwartet werden
+    do_something(argument_in, &argument_out1, &argument_out2);
+
+    printf("argument_out1: %lf\n", argument_out1);
+    printf("\n");
+    printf("argument_out2: %lf\n", argument_out2);
+    return 0;
+}
---
+++ Chapter06_Pointers/MoreAboutPointer2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 1;
+    int b = 2;
+    int c = 3;
+
+    int *p_d = &a; //Speicheradresse von a
+    int *p_e = &b; //Speicheradresse von b
+    int *p_f = &c; //Speicheradresse von c
+
+    //2 * 2 + 3 * 3 = 13
+    //13 wird in a gespeichert, da *p_d auf a zeigt (Zeile 9)
+    *p_d = (2 * *p_e) + (3 * *p_f);
+
+    p_d = &b; //p_d zeigt auf b
+    p_f = &a; //p_f zeigt auf a
+
+    //2 + 13 = 15
+    //15 wird in b gespeichert
+    *p_e = *p_d + *p_f;
+
+    printf("a: %d\n", a);
+    printf("*p_d: %d\n", *p_d);
+    printf("*p_e: %d\n", *p_e);
+    printf("*p_f: %d\n", *p_f);
+}
