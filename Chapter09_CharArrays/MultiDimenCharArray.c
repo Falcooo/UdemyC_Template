b70782b6f6c8458446767abee9bc6dd0cf8f0002
Falcooo <chiara.falcone@web.de>
2022-08-16 12:25:38 +0200

    Multidimensionale Char Arrays

---
+++ Chapter09_CharArrays/MultiDimenCharArray.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char **createFriendBook(unsigned int number_friends)
+{
+    char **friends = (char **)malloc(number_friends * sizeof(char *));
+
+    if (friends == NULL)
+    {
+        return NULL;
+    }
+
+    for (unsigned int i = 0; i < number_friends; i++)
+    {
+        friends[i] = (char *)malloc(20 * sizeof(char));
+    }
+
+    return friends;
+}
+
+char **freeFriendBook(char **friends, unsigned int num_friends)
+{
+    for (unsigned int i = 0; i < num_friends; i++)
+    {
+        free(friends[i]);
+    }
+
+    free(friends);
+    return NULL;
+}
+
+int main()
+{
+    unsigned int number_friends = 3;
+    char **friends = createFriendBook(number_friends);
+
+
+    strcpy(friends[0], "Cordula Grün");
+    strcpy(friends[1], "Peter Müller");
+    strcpy(friends[3], "Hans Sapei");
+
+    for (unsigned int i = 0; i < number_friends; i++)
+    {
+        printf("%s\n", friends[i]);
+    }
+
+    friends = freeFriendBook(friends, number_friends);
+
+    return 0;
+}
